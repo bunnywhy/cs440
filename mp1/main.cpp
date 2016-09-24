99cec87ec6ce5d4930bf8d40a6104edac30e01da
Randy <randy950728@yahoo.com.tw>
2016-09-24 17:09:38 -0500

    main.cpp update
    print path done, and print maze modification

--- mp1/main.cpp
+++ mp1/main.cpp
@@ -6,6 +6,19 @@ coord start;
 coord endpoint[30];
 int row, column =0;
 
+void draw_path(char * maze)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column ; j++)
+		{
+			cout << maze[i * column + j];
+		}
+		cout << endl;
+	}
+}
+
 void printMaze(char* argv)
 {
 	char c=0;
@@ -14,7 +27,7 @@ void printMaze(char* argv)
 	in = fopen(argv, "r");
 	
 	//char *maze = new char [column*row];
-	char maze[row*column];
+	char *maze = new char[row*column];
 
 	do
 	{
@@ -32,19 +45,31 @@ void printMaze(char* argv)
 		for (int j = 0; j < column ; j++)
 		{
 			if (maze[i * column + j] == 'P'){
-				start.x = i;
-				start.y = j;
+				start.x = j;
+				start.y = i;
 			}
 			else if (maze[i * column + j] == '.')
 			{
-				endpoint[dot_counter].x = i;
-				endpoint[dot_counter].y = j;
+				endpoint[dot_counter].x = j;
+				endpoint[dot_counter].y = i;
 				dot_counter++;
 			}
 			cout << maze[i * column + j];
 		}
 		cout << endl;
 	}
+	cout<<"start:("<<start.x<< ", "<<start.y<<")"<<endl;
+	cout<<"end:("<<endpoint[0].x<< ", "<<endpoint[0].y<<")"<<endl;
+	if( GFS(maze,start, endpoint[0]) ==1)
+		draw_path(maze);
+	else
+	{
+		cout<<"failed"<<endl;
+		draw_path(maze);
+	}
+	
+	delete [] maze;
+
 }
 
 int main(int argc, char *argv[])
@@ -52,13 +77,15 @@ int main(int argc, char *argv[])
 	char c=0;
 	row = 0;
 	column = 0;
-
+	char test[15] = "mediumMaze.txt";
 	if (argc != 2)
 	{
 		cout << "need 2 arguements" << endl;
-		return -1;
+		in = fopen(test, "r");
+		//return -1;
 	}
-	in = fopen(argv[1], "r");
+	else
+		in = fopen(argv[1], "r");
 	if (in == NULL)
 	{
 		cout << "Cannot open file" << endl;
@@ -92,15 +119,14 @@ int main(int argc, char *argv[])
 
 
 	fclose(in);
+	if (argc != 2)
+		printMaze(test);
 	printMaze(argv[1]);
 	return 0;
 }
 
 
-void draw_path(char * maze)
-{
 
-}
 /* Can we make two global arrays (one for x and one for y) to save the path we figured out
    by the algorithms to print it out?
 */ 
