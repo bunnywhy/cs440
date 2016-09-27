125d1c44f5dcb32021e8b79678a007dd504479c4
Randy <randy950728@yahoo.com.tw>
2016-09-26 23:34:56 -0500

    numerated?

--- mp1/a_star.cpp
+++ mp1/a_star.cpp
@@ -8,6 +8,73 @@ vector<coord> closedSet;
 map<coord, coord> cameFrom;
 map<coord, int> pathCost;
 #define debug 0
+int counter=0;
+char ca [62];
+void ca_init()
+{
+	ca[0]='0';
+	ca[1]='1';
+	ca[2]='2';
+	ca[3]='3';
+	ca[4]='4';
+	ca[5]='5';
+	ca[6]='6';
+	ca[7]='7';
+	ca[8]='8';
+	ca[9]='9';
+	ca[10]='a';
+	ca[11]='b';
+	ca[12]='c';
+	ca[13]='d';
+	ca[14]='e';
+	ca[15]='f';
+	ca[16]='g';
+	ca[17]='h';
+	ca[18]='i';
+	ca[19]='j';
+	ca[20]='k';
+	ca[21]='l';
+	ca[22]='m';
+	ca[23]='n';
+	ca[24]='o';
+	ca[25]='p';
+	ca[26]='q';
+	ca[27]='r';
+	ca[28]='s';
+	ca[29]='t';
+	ca[30]='u';
+	ca[31]='v';
+	ca[32]='w';
+	ca[33]='x';
+	ca[34]='y';
+	ca[35]='z';
+	ca[36]='A';
+	ca[37]='B';
+	ca[38]='C';
+	ca[39]='D';
+	ca[40]='E';
+	ca[41]='F';
+	ca[42]='G';
+	ca[43]='H';
+	ca[44]='I';
+	ca[45]='J';
+	ca[46]='K';
+	ca[47]='L';
+	ca[48]='M';
+	ca[49]='N';
+	ca[50]='O';
+	ca[51]='P';
+	ca[52]='Q';
+	ca[53]='R';
+	ca[54]='S';
+	ca[55]='T';
+	ca[56]='U';
+	ca[57]='V';
+	ca[58]='W';
+	ca[59]='X';
+	ca[60]='Y';
+	ca[61]='Z';
+}
 int optimal_cost(coord cur, coord end)
 {
 	int euc_val;
@@ -46,14 +113,14 @@ coord min_cost(vector<coord> openSet, map<coord, int> cost)
 void _draw_path(char * maze)
 {
 	int i = 0;
-	for (i = 0; i < row; i++)
+	/*for (i = 0; i < row; i++)
 	{
 		for (int j = 0; j < column; j++)
 		{
 			cout << maze[i * column + j];
 		}
 		cout << endl;
-	}
+	}*/
 }
 
 void find_neighbor(char* maze, coord current)
@@ -170,6 +237,7 @@ int _a_star(char *maze, coord start, vector<coord> endset)
 {
 	
 	openSet.push_back(start);
+	ca_init();
 	pathCost[start] = 0;
 	totalCost[start] = optimal_cost(start, endset[0]);
 	return _a_star_test(maze, start, endset);
@@ -186,7 +254,7 @@ int _a_star_test(char *maze, coord start, vector<coord> endset)
 
 	current.x=-1;
 	current.y=-1;
-
+	
 	//While frontier is not empty
 	while (!openSet.empty())
 	{
@@ -209,9 +277,10 @@ int _a_star_test(char *maze, coord start, vector<coord> endset)
 		current = min_cost(openSet, totalCost);
 		cameFrom[current] = parent;
 
-		if(debug&0)
-			maze[get_cord(current)] = 'x';
-
+		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!?/
+		maze[get_cord(current)] = ca[counter];
+		counter = (counter+1)%(10+26*2);
+		draw_ppath(maze);
 		//cout << "endpoint: " << end.x << " " << end.y << endl; 
 		//cout << "current.x:" << current.x << ' ' << "current.y" << current.y << endl;
 		//cout << "current cost:" << totalCost[current] << endl;
@@ -256,10 +325,10 @@ int _a_star_test(char *maze, coord start, vector<coord> endset)
 				prev_end = end;				
 			}
 
-			if(debug)
+			/*if(debug)
 			{
 				maze[get_cord(current)] = 'x';
-			}
+			}*/
 		}
 
 		//Move the current location out of the frontier
